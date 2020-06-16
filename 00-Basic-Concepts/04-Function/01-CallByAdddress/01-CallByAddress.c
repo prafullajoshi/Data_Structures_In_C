48cb74eab2d08e3fadcd622c29adf2e69d4148e6
Prafulla Joshi <joshiprafulla21@gmail.com>
2020-06-16 22:34:24 +0530

    Call By Address Program Added

---
+++ 00-Basic-Concepts/04-Function/01-CallByAdddress/01-CallByAddress.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+//Entry Point Function
+int main()
+{
+    //Local function declaration
+    void swap(int*, int*);
+    // Local variable declaration
+    int a, b;
+    a = 10;
+    b = 20;
+    printf("\nBefore swapping :");
+    printf("\nA :%d\nB :%d", a, b);
+    
+    swap(&a, &b);                                 // Actual Parameters
+    
+    printf("\nAfter swapping :");
+    printf("\nA :%d\nB :%d", a, b);
+
+    return 0;
+}
+
+void swap(int *x, int *y)                         // Formal Parameters
+{
+    //Local variable declaration
+    int temp;
+
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
